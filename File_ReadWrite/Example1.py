b6182a11ebf7b93464e5154a4537d3fd3789c63a
dharmishthakothari <dharmishtha.only@gmail.com>
2026-02-03 09:59:36 +0530

    Add files via upload

---
+++ File_ReadWrite/Example1.py
@@ -0,0 +1,9 @@
+
+path="C:\\Users\\Admin\\Documents\\9_Dec_Python\\Conditional_stmt\\example7.py"
+#file=open("C:\\Users\\Admin\\Documents\\Notes.txt")
+file=open(path)
+print(file)
+
+# read
+data=file.read()
+print(data)
